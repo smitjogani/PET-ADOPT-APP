d8a265abb92cf474c91dc8f1f857d63730d2e16a
smit jogani <smitjogani00@gmail.com>
2024-08-27 11:03:22 +0530

    Authentication using Clerk

---
+++ app/_layout.jsx
@@ -0,0 +1,56 @@
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key)
+      if (item) {
+        console.log(`${key} was used 🔐 \n`)
+      } else {
+        console.log('No values stored under key: ' + key)
+      }
+      return item
+    } catch (error) {
+      console.error('SecureStore get item error: ', error)
+      await SecureStore.deleteItemAsync(key)
+      return null
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  },
+}
+
+
+export default function RootLayout() {
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+  useFonts({
+    "outfit-bold": require("@/assets/fonts/Outfit-Bold.ttf"),
+    "outfit-medium": require("@/assets/fonts/Outfit-Medium.ttf"),
+    outfit: require("@/assets/fonts/Outfit-Regular.ttf"),
+  });
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+}
